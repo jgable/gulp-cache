2773e3b242cd6462ce6affa8c65122d210df50cb
dangreen <danon0404@gmail.com>
2019-06-08 14:50:14 +0700

    deps upd; trigen-scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
+import {
+	external
+} from '@trigen/scripts-plugin-rollup/helpers';
+import { eslint } from 'rollup-plugin-eslint';
+import json from 'rollup-plugin-json';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import json from 'rollup-plugin-json';
-import { eslint } from 'rollup-plugin-eslint';
 import pkg from './package.json';
 
 const plugins = [
 	eslint({
 		exclude:      ['**/*.json', 'node_modules/**'],
-		throwOnError: process.env.ROLLUP_WATCH != 'true'
+		throwOnError: true
 	}),
 	json({
 		preferConst: true
@@ -17,22 +20,12 @@ const plugins = [
 		runtimeHelpers: true
 	})
 ];
-const dependencies = [].concat(
-	['crypto', 'stream', 'events', 'buffer', 'util'],
-	Object.keys(pkg.dependencies)
-);
-
-function external(id) {
-	return dependencies.some(_ =>
-		_ == id || id.indexOf(`${_}/`) == 0
-	);
-}
 
 export default {
-	input:  'src/index.js',
+	input:    'src/index.js',
 	plugins,
-	external,
-	output: {
+	external: external(pkg, true),
+	output:   {
 		file:      pkg.main,
 		format:    'cjs',
 		sourcemap: 'inline'
